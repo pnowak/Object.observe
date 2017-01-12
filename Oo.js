a0b23ddf623821590c8c814f3ee9486336666217
pnowak <piotr.nowak.25.02.1978@gmail.com>
2017-01-12 22:21:08 +0100

    step 3

--- Oo.js
+++ Oo.js
@@ -5,7 +5,7 @@ const mandatory = (parameter) => {
 export class Observe {
     constructor(target, fn = mandatory('callback'), changes = ['add', 'update', 'delete', 'reconfigure', 'setPrototype', 'preventExtensions']) {
 
-        this.changes = changes;
+        this.fn = fn;
 
         const baseDescriptor = {
             configurable: false,
@@ -20,6 +20,7 @@ export class Observe {
 
     get(target, key, context) {
         if (Reflect.has(target, key)) {
+            this.fn.apply(this, [{ name: key, object: JSON.stringify(target), type: 'check' }]);
             return Reflect.get(target, key, context);
         }
         else {
@@ -28,28 +29,40 @@ export class Observe {
     }
 
     set(target, key, value, context) {
-        if (Reflect.has( target, key )) {
-            console.log({ name: key, object: JSON.stringify(target), type: 'update', oldValue: target[key] });
-            return Reflect.set(target, key, value, context);
-        }
-        else {
-            console.log({ name: key, object: JSON.stringify(target), type: 'add' });
-            return Reflect.set(target, key, value, context);
+        if (Reflect.isExtensible(target)) {
+            if (Reflect.has( target, key )) {
+                this.fn.apply(this, [{ name: key, object: JSON.stringify(target), type: 'update', oldValue: target[key] }]);
+                return Reflect.set(target, key, value, context);
+            }
+            else {
+                this.fn.apply(this, [{ name: key, object: JSON.stringify(target), type: 'add' }]);
+                return Reflect.set(target, key, value, context);
+            }
+        } else {
+            throw new TypeError(`${target} is not extensible`);
         }
     }
 
     deleteProperty(target, key) {
-        console.log({ name: key, object: JSON.stringify(target), type: 'delete', oldValue: target[key] });
-        return Reflect.deleteProperty(target, key);
+        if (Reflect.isExtensible(target)) {
+            this.fn.apply(this, [{ name: key, object: JSON.stringify(target), type: 'delete', oldValue: target[key] }]);
+            return Reflect.deleteProperty(target, key);
+        } else {
+            throw new TypeError(`${target} is not extensible`);
+        }
     }
 
     defineProperty(target, key, desc) {
-        console.log({name: key, object: JSON.stringify(target), type: 'reconfigure', descriptor: JSON.stringify(desc) });
-        return Reflect.defineProperty(target, key, desc);
+        if (Reflect.isExtensible(target)) {
+            this.fn.apply(this, [{ name: key, object: JSON.stringify(target), type: 'reconfigure', descriptor: JSON.stringify(desc) }]);
+            return Reflect.defineProperty(target, key, desc);
+        } else {
+            throw new TypeError(`${target} is not extensible`);
+        }
     }
 
     setPrototypeOf(target, proto) {
-        console.log({ name: JSON.stringify(proto), object: JSON.stringify(target), type: 'setPrototype', oldValue: JSON.stringify(target) });
+        this.fn.apply(this, [{ name: JSON.stringify(proto), object: JSON.stringify(target), type: 'setPrototype', oldValue: JSON.stringify(target) }]);
         return Reflect.setPrototypeOf(target, proto);
     }
 }
@@ -60,3 +73,7 @@ Reflect.defineProperty(Observe.prototype, Symbol.toStringTag, {
     enumerable: false,
     value: 'Observe',
 });
+
+// gdy zrobimy na przykład tak, że
+// Object.defineProperty(oo, 'a', { writable: false });
+// przy próbie nadpisania takiej własności powinniśmy otrzymać informację, iż jest to niemożliwie bo..
